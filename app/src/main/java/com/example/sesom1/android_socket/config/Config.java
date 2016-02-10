0e7c4dd47babece86d542ff9e5eb88baf22d7e09
se-som <se.somitc41@gmail.com>
2016-02-10 16:30:13 +0700

    some change

--- app/src/main/java/com/example/sesom1/android_socket/config/Config.java
+++ app/src/main/java/com/example/sesom1/android_socket/config/Config.java
@@ -6,6 +6,5 @@ package com.example.sesom1.android_socket.config;
 public final class Config {
     public static final class VALUE{
         public static final String URL = "http://192.168.1.115:3000";
-		///////////////////
     }
 }
